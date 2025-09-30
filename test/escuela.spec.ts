8bd8291ae0b60ed94ca55005f954536cacd5213e
William Espindola Cardoso <williamcardoso_sc@hotmail.com>
2025-09-30 19:57:30 -0300

    atualizando exercicios prova

--- test/escuela.spec.ts
+++ test/escuela.spec.ts
@@ -7,17 +7,17 @@ describe('Escuela API', () => {
   const rep = SimpleReporter;
   const baseUrl = 'https://api.escuelajs.co/api/v1';
   let userId = '';
-  let productId = '1556';
+  let productId = '';
 
   p.request.setDefaultTimeout(30000);
 
   beforeAll(() => p.reporter.add(rep));
   afterAll(() => p.reporter.end());
 
-    describe('User', () => {
-
-// Usuário
-    it('Cadastra um novo usuário', async () => {
+  describe('User', () => {
+// Testes Positivos \\
+// Teste de Usuário
+    it('Cadastra o novo usuário', async () => {
       userId = await p
         .spec()
         .post(`${baseUrl}/users`)
@@ -29,18 +29,18 @@ describe('Escuela API', () => {
           avatar: 'https://www.google.com.br/'
         })
         .expectStatus(StatusCodes.CREATED)
-        .returns('id').inspect();
-        console.log(userId)
+        .returns('id');
+        // console.log(userId)
     });
 
-    it('Busca um usuário cadastrado', async () => {
+    it('Busca o usuário cadastrado', async () => {
       await p
         .spec()
         .get(`${baseUrl}/users/${userId}`)
         .expectStatus(StatusCodes.OK)
     });
     
-    it('Atualiza os dados um usuário cadastrado', async () => {
+    it('Atualiza os dados do usuário cadastrado', async () => {
       await p
         .spec()
         .put(`${baseUrl}/users/${userId}`)
@@ -60,11 +60,28 @@ describe('Escuela API', () => {
         .delete(`${baseUrl}/users/${userId}`)
         .expectStatus(StatusCodes.OK)
     });
-    
+
+    it('Cadastra o novo usuário', async () => {
+      userId = await p
+        .spec()
+        .post(`${baseUrl}/users`)
+        .withJson({
+          email: 'william@gmail.com',
+          name: 'william',
+          password: '12345',
+          role: 'admin',
+          avatar: 'https://www.google.com/imgres?q=avatar%20homem%20de%20barba&imgurl=https%3A%2F%2Fimg.freepik.com%2Fvetores-premium%2Fretrato-de-um-jovem-com-barba-e-estilo-de-cabelo-avatar-masculino-ilustracao-vetorial_266660-423.jpg&imgrefurl=https%3A%2F%2Fbr.freepik.com%2Fvetores-premium%2Fretrato-de-um-jovem-com-barba-e-estilo-de-cabelo-avatar-masculino-ilustracao-vetorial_19282811.htm&docid=KlUmnwAJc2R-pM&tbnid=Doetq6jcA2UJlM&vet=12ahUKEwiZhKTsyIGQAxXctpUCHU3UNj8QM3oECCYQAA..i&w=626&h=626&hcb=2&ved=2ahUKEwiZhKTsyIGQAxXctpUCHU3UNj8QM3oECCYQAA'
+        })
+        .expectStatus(StatusCodes.CREATED)
+        .returns('id');
+        // console.log(userId)
+    });
+  });
 
 
-// Produto
-    it('Cadastra um novo Produto', async () => {
+// Teste de Produto
+  describe('Products', () => {
+    it('Cadastra o novo Produto', async () => {
       productId = await p
         .spec()
         .post(`${baseUrl}/products`)
@@ -78,18 +95,18 @@ describe('Escuela API', () => {
           ]
             })
         .expectStatus(StatusCodes.CREATED)
-        .returns('id').inspect();
-        console.log(productId)
+        .returns('id');
+        // console.log(productId)
     });
 
-    it('Busca um produto cadastrado', async () => {
+    it('Busca o produto cadastrado', async () => {
       await p
         .spec()
         .get(`${baseUrl}/products/${productId}`)
         .expectStatus(StatusCodes.OK)
     });
 
-    it('Atualiza informações de um produto cadastrado', async () => {
+    it('Atualiza informações do produto cadastrado', async () => {
       await p
         .spec()
         .put(`${baseUrl}/products/${productId}`)
@@ -111,6 +128,59 @@ describe('Escuela API', () => {
         .delete(`${baseUrl}/products/${productId}`)
         .expectStatus(StatusCodes.OK)
     });
-    
+
+    it('Cadastra o novo Produto', async () => {
+      productId = await p
+        .spec()
+        .post(`${baseUrl}/products`)
+        .withJson({
+          "title": "Sofa Retratil",
+          "price": 699.00,
+          "description": "Sofa 3 Lugares",
+          "categoryId": 3,
+          "images": [
+            "https://www.google.com/imgres?q=Sofa%203%20Lugares&imgurl=https%3A%2F%2Freidosestofados.com.br%2Fwp-content%2Fuploads%2F15183502104_Sofa20Retratil202.0020m20-20Modelo20Compact20-20Cinza202022020-200-1.jpg&imgrefurl=https%3A%2F%2Freidosestofados.com.br%2Fproduto%2Fsofa-3-lugares-retratil-e-reclinavel-compact-200m-velusoft-cinza-202%2F&docid=uOkhT-TmVskqiM&tbnid=_BgcC1d8xG1yAM&vet=12ahUKEwiH0-K_yYGQAxVatJUCHTVDB-IQM3oECBMQAA..i&w=711&h=473&hcb=2&ved=2ahUKEwiH0-K_yYGQAxVatJUCHTVDB-IQM3oECBMQAA"
+          ]
+            })
+        .expectStatus(StatusCodes.CREATED)
+        .returns('id');
+        // console.log(productId)
+    });
+  });
+
+
+// Testes Negativos
+
+  describe('Testes Negativos', () => {
+    it('Cadastro de um novo Produto com valor negativo', async () => {
+      await p
+        .spec()
+        .post(`${baseUrl}/products`)
+        .withJson({
+          "title": "Cadeira",
+          "price": -249.99,
+          "description": "Cadeira colorida",
+          "categoryId": 2,
+          "images": [
+            "https://www.primaxmoveis.com.br/kit-4-cadeira-infantil-colorida-polipropileno-empilhavel-cas/p/MLB43905471"
+          ]
+            })
+        .expectStatus(StatusCodes.BAD_REQUEST)
+        .returns('id');
+    });
+
+    it('Não deve atualizar produto com ID inválido', async () => {
+      await p
+        .spec()
+        .put(`${baseUrl}/products/9999`)
+        .withJson({
+          "title": "Produto Teste",
+          "price": 1000,
+          "description": "Teste de erro",
+          "categoryId": 3,
+          "images": ["https://placeimg.com/640/480/any"]
+        })
+        .expectStatus(StatusCodes.BAD_REQUEST);
+    });
   });
 });
